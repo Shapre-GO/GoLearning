4e01cb277f4ecff8d1a80b8e691e6b432d3c398e
Shapre-GO <baimurzin.tim@yandex.ru>
2026-03-26 03:21:02 +0300

    Delete Learn1 directory

--- Learn1/errors.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "errors"
-
-var (
-	ErrNameTooShort = errors.New("Failed: Name is too short")
-	ErrInvalidEmail = errors.New("Failed: Email is invalid")
-	ErrEmailTaken   = errors.New("Failed: This email already exists")
-)
